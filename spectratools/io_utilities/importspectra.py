02524c41e8e44c60c5abdeee9156d2403f2cfba1
Chiara Tomaiuolo <ctomaiuolo@caen.it>
2025-01-16 15:24:07 +0100

    Refactor and enhance spectrum import utilities with format detection

--- spectratools/io_utilities/importspectra.py
+++ spectratools/io_utilities/importspectra.py
@@ -1,8 +1,8 @@
 """Utilities for importing spectra from various file formats.
    The avaliable formats (by now) are:
-   - .txt
-   - .csv
-   - .root 
+   - .txt [x]
+   - .csv [x]
+   - .root [x]
 """
 
 # System libraries first...
@@ -27,12 +27,17 @@ def import_from_txt(file_path: str | os.PathLike) -> Tuple[np.array, np.array]:
     bins, content : Tuple[np.array, np.array]
         Numpy arrays containing the bin limits and their content.
     """
-    try:
-        bins, content = np.loadtxt(file_path, unpack=True)
-        return bins, content
-    except ValueError:
-        bins, content, error = np.loadtxt(file_path, unpack=True)
-        return bins, content
+    data = np.loadtxt(file_path)
+    if data.shape[1] == 1: # This is the case of GammaStream MC2 acquired histo
+        bins, content = np.arange(data.shape[0]), data
+    elif data.shape[1] == 2: # This is a standard bin, content format
+        bins, content = data[:,0], data[:,1]
+    elif data.shape[1] == 3: # This is a bin, content, error format
+        bins, content = data[:,0], data[:,1]
+    else: # This is an unknown format
+        print("Unknown format.")
+        sys.exit(1)
+    return bins, content
 
 def import_from_csv(file_path: str | os.PathLike) -> Tuple[np.array, np.array]:
     """Import a spectrum from a .csv file.
@@ -47,12 +52,18 @@ def import_from_csv(file_path: str | os.PathLike) -> Tuple[np.array, np.array]:
     bins, content : Tuple[np.array, np.array]
         Numpy arrays containing the bin limits and their content.
     """
-    try:
-        bins, content = np.loadtxt(file_path, delimiter=',', unpack=True)
-        return bins, content
-    except ValueError:
-        bins, content, error = np.loadtxt(file_path, unpack=True)
-        return bins, content
+    data = np.loadtxt(file_path, delimiter=',')
+
+    if data.shape[1] == 1: # This is the case of GammaStream MC2 acquired histo
+        bins, content = np.arange(data.shape[0]), data
+    elif data.shape[1] == 2: # This is a standard bin, content format
+        bins, content = data[:,0], data[:,1]
+    elif data.shape[1] == 3: # This is a bin, content, error format
+        bins, content = data[:,0], data[:,1]
+    else: # This is an unknown format
+        print("Unknown format.")
+        sys.exit(1)
+    return bins, content
     
 def import_from_root(file_path: str | os.PathLike, treename: str='Data_R') -> Tuple[np.array, np.array]:
     """Import a spectrum from a .root file. The default name of the tree is 'Data_R',
@@ -82,4 +93,15 @@ def import_from_root(file_path: str | os.PathLike, treename: str='Data_R') -> Tu
     # Default number of bins is 4096 (CoMPASS acquisition)
     hist, bin_edges = np.histogram(spectrum, bins=4096)
     return hist, bin_edges
+
+def import_spectrum(file_path: str | os.PathLike) -> Tuple[np.array, np.array]:
+    if file_path.endswith('.txt'):
+        return import_from_txt(file_path)
+    elif file_path.endswith('.csv'):
+        return import_from_csv(file_path)
+    elif file_path.endswith('.root'):
+        return import_from_root(file_path)
+    else:
+        print("File format not supported.")
+        sys.exit(1)
 
