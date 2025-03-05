3c2878da5ceef208702312abe5c0eab300ab5eee
Chiara Tomaiuolo <ctomaiuolo@caen.it>
2025-03-05 15:57:30 +0100

    Added script for executable creation

---
+++ scripts/create_executable.py
@@ -0,0 +1,12 @@
+# scripts/create_executable.py
+
+import PyInstaller.__main__
+
+PyInstaller.__main__.run([
+    'main.py',  # Il percorso al file principale del tuo progetto
+    '--onefile',           # Crea un singolo file eseguibile
+    '--name=VisualizeSpectra',  # Nome dell'eseguibile
+    '--distpath=dist',     # Directory di output
+])
+
+#print("Eseguibile creato in: dist/VisualizeSpectra")
